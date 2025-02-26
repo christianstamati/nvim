42b087821ba487f0dd55493572c51e052c6c0e54
Christian Stamati <c.stamati@we-wear.biz>
2025-02-26 23:34:05 +0100

    updated toggle term

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,8 @@
+return {
+  "akinsho/toggleterm.nvim",
+  opts = {
+    shell = [["C:\\Program Files\\Git\\bin\\bash.exe"]], -- Properly quoted path
+    shade_terminals = true,
+    persist_size = true,
+  },
+}
